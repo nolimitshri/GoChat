2accdad42689688804d4244c5beebba4f3a9d930
nolimitshri <shri04052003@gmail.com>
2022-01-01 12:56:37 +0530

    updated the code for deployment

--- frontend/js/client.js
+++ frontend/js/client.js
@@ -1,4 +1,5 @@
-const socket = io('http://localhost:8000');
+// const socket = io('http://localhost:8000');
+const socket = io("https://thawing-lowlands-90887.herokuapp.com/");
 
 // get DOM elements in respective JS variables
 const form = document.getElementById("send-container");
